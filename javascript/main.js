355174e6b5abb6c9ab87f97fa60c59aa26b0a0f4
danilaurenhill <dani.lauren.hill@gmail.com>
2016-03-02 16:51:11 -0500

    WIP remove extras

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,7 @@
 function hideshow(content1) {
-
-    if (!document.getElementById) return
-    if (content1.style.display == "none") content1.style.display = "block"
-    else content1.style.display = "none"
+  if (!document.getElementById) return
+  if (content1.style.display == "none") content1.style.display = "block"
+  else content1.style.display = "none"
 }
 
 // function hideshow(content1) {
